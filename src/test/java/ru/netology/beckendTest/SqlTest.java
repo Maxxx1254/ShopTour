6fcd867dc4f1c6b4698cd28a7d6d64427c616d72
Maxxx1254 <96447360+Maxxx1254@users.noreply.github.com>
2022-11-22 15:58:37 +0300

    перенес формирование тела в хелпер

--- src/test/java/ru/netology/beckendTest/SqlTest.java
+++ src/test/java/ru/netology/beckendTest/SqlTest.java
@@ -4,9 +4,7 @@ import org.testng.annotations.Test;
 import ru.netology.helpers.SQLHelper;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,13 +13,9 @@ public class SqlTest {
 
     @Test
     public void declined() throws IOException, InterruptedException {
-        var request = HttpRequest.newBuilder()
-                .uri(URI.create(SQLHelper.getEndpoint()))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(SQLHelper.getBodyRequestDeclined()))
-                .build();
+        SQLHelper.getBodyRequestDeclined();
         var client = HttpClient.newHttpClient();
-        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        var response = client.send(SQLHelper.requestDeclined(), HttpResponse.BodyHandlers.ofString());
         System.out.println(response.statusCode());
         System.out.println(response.body());
         assertEquals("{\"status\":\"DECLINED\"}", response.body());
@@ -29,13 +23,8 @@ public class SqlTest {
 
     @Test
     public void approved() throws IOException, InterruptedException {
-        var request = HttpRequest.newBuilder()
-                .uri(URI.create(SQLHelper.getEndpoint()))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(SQLHelper.getBodyRequestApproved()))
-                .build();
         var client = HttpClient.newHttpClient();
-        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        var response = client.send(SQLHelper.requestApproved(), HttpResponse.BodyHandlers.ofString());
         System.out.println(response.statusCode());
         System.out.println(response.body());
         assertEquals("{\"status\":\"APPROVED\"}", response.body());
@@ -43,13 +32,8 @@ public class SqlTest {
 
     @Test
     public void internalServerError() throws IOException, InterruptedException {
-        var request = HttpRequest.newBuilder()
-                .uri(URI.create(SQLHelper.getEndpoint()))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(SQLHelper.getBodyRequestInternalServerError()))
-                .build();
         var client = HttpClient.newHttpClient();
-        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        var response = client.send(SQLHelper.badRequest(), HttpResponse.BodyHandlers.ofString());
         System.out.println(response.statusCode());
         assertEquals(500, response.statusCode());
     }
--- src/test/java/ru/netology/helpers/SQLHelper.java
+++ src/test/java/ru/netology/helpers/SQLHelper.java
@@ -1,5 +1,8 @@
 package ru.netology.helpers;
 
+import java.net.URI;
+import java.net.http.HttpRequest;
+
 public class SQLHelper {
     public static String getEndpoint() {
         return "http://localhost:8080/api/v1/pay";
@@ -32,5 +35,27 @@ public class SQLHelper {
                 "  \"holder\": \"Varlamov Ilya\",\n" +
                 "  \"cvc\": " + UserHelper.getThreeNumber() + "\n" +
                 "}";
+    }
+
+    public static HttpRequest requestApproved() {
+        return HttpRequest.newBuilder()
+                .uri(URI.create(SQLHelper.getEndpoint()))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(SQLHelper.getBodyRequestApproved()))
+                .build();
+    }
+    public static HttpRequest requestDeclined() {
+        return HttpRequest.newBuilder()
+                .uri(URI.create(SQLHelper.getEndpoint()))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(SQLHelper.getBodyRequestDeclined()))
+                .build();
+    }
+    public static HttpRequest badRequest() {
+        return HttpRequest.newBuilder()
+                .uri(URI.create(SQLHelper.getEndpoint()))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(SQLHelper.getBodyRequestInternalServerError()))
+                .build();
     }
 }
